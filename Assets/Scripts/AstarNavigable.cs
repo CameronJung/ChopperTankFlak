f72e8def076ab2b276644df1d4427072578e1b8f
CameronJung <c.jung@mymts.net>
2024-04-29 15:25:01 -0500

    Created Level small battle
     - Created a new level "Operation Argument"
     - Changed tile debug test to only display when run in editor
     - Added another mountain tile variant

--- Assets/Scripts/AstarNavigable.cs
+++ Assets/Scripts/AstarNavigable.cs
@@ -47,9 +47,7 @@ public class AstarNavigable : MonoBehaviour
         //DebugText.text = cubePosition.ToString();
         DebugText.text = gridCoords.ToString();
 
-#if UNITY_WEBGL
-        DebugText.gameObject.SetActive(false);
-#endif
+        DebugText.gameObject.SetActive(Application.isEditor);
     }
 
     public void Awake()
@@ -77,9 +75,10 @@ public class AstarNavigable : MonoBehaviour
 
     public void ChangeDebugTextTo(string words)
     {
-#if UNITY_WEBGL
-        DebugText.text = words;
-#endif
+        if (Application.isEditor)
+        {
+            DebugText.text = words;
+        }
     }
 
 
@@ -100,10 +99,11 @@ public class AstarNavigable : MonoBehaviour
         GValue = int.MaxValue;
         HValue = int.MaxValue;
 
-
-#if UNITY_WEBGL
-        DebugText.text = (OnHex.myCoords).ToString();
-#endif
+        if (Application.isEditor)
+        {
+            DebugText.text = (OnHex.myCoords).ToString();
+        }
+        
     }
 
     public int GetFValue()
