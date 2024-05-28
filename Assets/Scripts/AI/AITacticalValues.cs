077ec3b9cb295d4a838a2585b49df8ee409457c1
CameronJung <c.jung@mymts.net>
2024-05-28 14:03:58 -0500

    Created Operation Castle
     - Created a level, Operation Castle
     - Closes #19
     - Changed some AI values to make the AI more aggressive
     - Cleared out some redundant assets

--- Assets/Scripts/AI/AITacticalValues.cs
+++ Assets/Scripts/AI/AITacticalValues.cs
@@ -9,15 +9,15 @@ public static class AITacticalValues
      */
 
     //Combat values
-    public const int DESTROYS_VEHICLE = 5;
+    public const int DESTROYS_VEHICLE = 6;
     public const int DESTROYS_INFANTRY = 4;
     public const int STARTS_STALEMATE = 3;
     public const int RESOLVES_STALEMATE = 3;
-    public const int WILL_BE_DESTROYED = -5;
+    public const int WILL_BE_DESTROYED = -7;
     public const int INFANTRY_ENDS_STALEMATE = 2;
 
     //Threat analysis
-    public const int MORTAL_DANGER = -5;
+    public const int MORTAL_DANGER = -4;
     public const int MILD_DANGER = -1;
     public const int OPPORTUNITY = +2;
 
@@ -25,8 +25,8 @@ public static class AITacticalValues
     public const int MARCH_ON = 0;
     public const int MOVES_MAXIMUM = 1;
     public const int AWAY_FROM_HOME = 1;
-    public const int CLOSER_TO_ENEMY_BASE = 2;
-    public const int INFANTRY_CLOSER_TO_ENEMY_BASE = 1;
+    public const int CLOSER_TO_ENEMY_BASE = 3;
+    public const int INFANTRY_CLOSER_TO_ENEMY_BASE = 2;
     public const int INFANTRY_CAPTURES_BASE = 15;
 
     //Bounties
